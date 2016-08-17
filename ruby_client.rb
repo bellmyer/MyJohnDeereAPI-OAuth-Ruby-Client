6d6875bd005da0b53d0c17f2d477c80db0d165dd
Gordon Wang <grdnwng@users.noreply.github.com>
2016-08-17 10:09:47 -0500

    Update link

--- ruby_client.rb
+++ ruby_client.rb
@@ -37,7 +37,7 @@ setupLinksForOauth(getBasicCatalog)
 
 consumer=OAuth::Consumer.new API_KEY,
 API_SECRET,
-{ :site => 'https://apicert.soa-proxy.deere.com',
+{ :site => 'https://sandboxapi.deere.com',
 :header =>{:Accept =>'application/vnd.deere.axiom.v3+json'},
 :http_method => :get,
 :request_token_url=>@requestToken_uri,
